7e0e0ac2beaa67e3866dbd4c2402ebb93c72e25c
nillsn <74359416+nillsn@users.noreply.github.com>
2022-02-25 16:06:14 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-import RouteController from './routeController.js';
-import ComponentBuilder from './componentBuilder.js';
-
-RouteController.validateRoute();
-
-customElements.define('app-component', ComponentBuilder);
