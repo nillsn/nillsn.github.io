631cdcc6bc3b89137278b2ebb38d6ae0828448c6
nillsn <74359416+nillsn@users.noreply.github.com>
2022-02-25 16:05:25 +0100

    Delete test directory

--- test/testCaller.js
+++
@@ -1,9 +0,0 @@
-import { describe } from 'mocha';
-
-export default class TestCaller {
-  static call(classUnderTest, methodUnderTest, tests) {
-    describe(`Class: ${classUnderTest} - Method: ${methodUnderTest}`, () => {
-      tests();
-    });
-  }
-}
