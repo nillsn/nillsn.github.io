caf643aee33116aeecdff87793bb432275548700
Philipp <>
2024-02-02 20:09:27 +0100

    bookmark-viewer

---
+++ bookmark-viewer/bookmarks.js
@@ -0,0 +1,19 @@
+export const BOOKMARK_COLLECTIONS = [
+    {
+        name : "Darts",
+        bookmarks : [
+            { name: "/r/Darts", url: "https://www.reddit.com/r/Darts/new/" },
+            { name: "Nillsn's App", url: "https://nillsn.github.io/next/" },
+            { name: "DSVN Competition", url: "https://dsvn.de/competition/" },
+            { name: "2K Software", url: "https://2k-dart-software.com/frontend/events" },
+            { name: "nuLiga - Bezirksklasse E", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2023%2F24&group=210162" },
+            { name: "nuLiga - Mannschaften", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/clubTeams?club=108" }
+        ]
+    },
+    {
+        name : "Others",
+        bookmarks : [
+            { name: "BBK Radio1", url: "https://www.bbc.co.uk/sounds/play/live:bbc_radio_one" }
+        ]
+    },
+];
