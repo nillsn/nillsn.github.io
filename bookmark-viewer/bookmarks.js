1dbb77625540ddc23e9cc8a38e584fd27d578f1b
Philipp <>
2024-03-28 14:02:31 +0100

    updated bookmarks

--- bookmark-viewer/bookmarks.js
+++ bookmark-viewer/bookmarks.js
@@ -5,17 +5,46 @@ export const BOOKMARK_COLLECTIONS = [
             { name: "/r/Darts", url: "https://www.reddit.com/r/Darts/new/" },
             { name: "Nillsn's App", url: "https://nillsn.github.io/next/" },
             { name: "DSVN Competition", url: "https://dsvn.de/competition/" },
-            { name: "2K Software", url: "https://2k-dart-software.com/frontend/events" },
-            { name: "Finnigan's Harp Rookie League", url: "https://2k-dart-software.com/frontend/events/1/event/10187/phase/17742/group/119742" },
-            { name: "nuLiga - Bezirksklasse E", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2023%2F24&group=210162" },
-            { name: "nuLiga - Damen", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2023%2F24&group=210165" },
-            { name: "nuLiga - Mannschaften", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/clubTeams?club=108" }
         ]
     },
     {
-        name : "Others",
+        name : "MOFDV",
         bookmarks : [
-            { name: "BBC Radio1", url: "https://www.bbc.co.uk/sounds/play/live:bbc_radio_one" }
+            { name: "Bezirksklasse E", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2023%2F24&group=210162" },
+            { name: "Damen", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2023%2F24&group=210165" },
+            { name: "DSVN Mannschaften", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/clubTeams?club=108" }
         ]
     },
+    {
+        name: "2K Software",
+        bookmarks: [
+            { name: "Events", url: "https://2k-dart-software.com/frontend/events" },
+            { name: "DSV Nürnberg 93", url: "https://2k-dart-software.com/frontend/events/2/mandant/1422" },
+            { name: "Heinrich VIII.", url: "https://2k-dart-software.com/frontend/events/4/mandant/452" },
+            { name: "DSV Finnigan's Harp", url: "https://2k-dart-software.com/frontend/events/1/mandant/393" }
+        ]
+    },
+    {
+        name: "Finnigan's Harp",
+        bookmarks: [
+            { name: "Rookie League", url: "https://2k-dart-software.com/frontend/events/1/event/10187/phase/17742/group/119742" },
+            { name: "Challenger League", url: "https://2k-dart-software.com/frontend/events/1/event/10186/participants" },
+            { name: "Premier League", url: "https://2k-dart-software.com/frontend/events/1/event/9586/participants" }
+        ]
+    },
+    {
+        name : "Radio",
+        bookmarks : [
+            { name: "BBC Radio1", url: "https://www.bbc.co.uk/sounds/play/live:bbc_radio_one" },
+            { name: "FM 104", url: "https://www.fm104.ie/player" },
+            { name: "ENERGY Nürnberg", url: "https://www.energy.de/nuernberg" },
+            { name: "Hit Radio N1", url: "https://radioplayer.hitradion1.de/livestream/" }
+        ]
+    },
+    {
+        name: "YouTube",
+        bookmarks: [
+            { name: "Liquid Drum and Base Mix", url: "https://www.youtube.com/watch?v=aJoo79OwZEI" }
+        ]
+    }
 ];
