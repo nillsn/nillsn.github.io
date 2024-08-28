4fa4bfe02e49ec32886ed7b86ea2faceaaec14c9
Philipp <>
2024-08-28 20:48:03 +0200

    edited bookmarks.js

--- bookmark-viewer/bookmarks.js
+++ bookmark-viewer/bookmarks.js
@@ -1,6 +1,6 @@
 export const BOOKMARK_COLLECTIONS = [
     {
-        name : "Darts",
+        name : "Darts Allgemein",
         bookmarks : [
             { name: "/r/Darts", url: "https://www.reddit.com/r/Darts/new/" },
             { name: "Nillsn's App", url: "https://nillsn.github.io/next/" },
@@ -8,11 +8,12 @@ export const BOOKMARK_COLLECTIONS = [
         ]
     },
     {
-        name : "MOFDV",
+        name :"NuLiga",
         bookmarks : [
-            { name: "Bezirksklasse E", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2023%2F24&group=210162" },
-            { name: "Damen", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2023%2F24&group=210165" },
-            { name: "DSVN Mannschaften", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/clubTeams?club=108" }
+            { name : "MOFDV 2024/25 ", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/leaguePage?championship=MOFDV+2024%2F25" },
+            { name : "Bezirksklasse F", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2024%2F25&group=210600" },
+            { name : "Verbandsliga Damen A", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2024%2F25&group=210576" },
+            { name : "Verbandsliga Damen B", url: "https://bdv-dart.liga.nu/cgi-bin/WebObjects/nuLigaDARTDE.woa/wa/groupPage?championship=MOFDV+2024%2F25&group=210388" }
         ]
     },
     {
@@ -21,15 +22,8 @@ export const BOOKMARK_COLLECTIONS = [
             { name: "Events", url: "https://2k-dart-software.com/frontend/events" },
             { name: "DSV Nürnberg 93", url: "https://2k-dart-software.com/frontend/events/2/mandant/1422" },
             { name: "Heinrich VIII.", url: "https://2k-dart-software.com/frontend/events/4/mandant/452" },
-            { name: "DSV Finnigan's Harp", url: "https://2k-dart-software.com/frontend/events/1/mandant/393" }
-        ]
-    },
-    {
-        name: "Finnigan's Harp",
-        bookmarks: [
-            { name: "Rookie League", url: "https://2k-dart-software.com/frontend/events/1/event/10187/phase/17742/group/119742" },
-            { name: "Challenger League", url: "https://2k-dart-software.com/frontend/events/1/event/10186/participants" },
-            { name: "Premier League", url: "https://2k-dart-software.com/frontend/events/1/event/9586/participants" }
+            { name: "DSV Finnigan's Harp", url: "https://2k-dart-software.com/frontend/events/1/mandant/393" },
+            { name: "Rookie League / Challenger League", url: "https://2k-dart-software.com/frontend/events/1/event/10187/phase/17742/group/119742" }
         ]
     },
     {
@@ -38,7 +32,8 @@ export const BOOKMARK_COLLECTIONS = [
             { name: "BBC Radio1", url: "https://www.bbc.co.uk/sounds/play/live:bbc_radio_one" },
             { name: "FM 104", url: "https://www.fm104.ie/player" },
             { name: "ENERGY Nürnberg", url: "https://www.energy.de/nuernberg" },
-            { name: "Hit Radio N1", url: "https://radioplayer.hitradion1.de/livestream/" }
+            { name: "Hit Radio N1", url: "https://radioplayer.hitradion1.de/livestream/" },
+            { name: "Clouds Radio", url: "https://radio.lofi-clouds.com/" }
         ]
     },
     {
